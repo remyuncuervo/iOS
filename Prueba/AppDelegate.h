73a2e61dd00a89ac212e6d25d46f37624a32d5a3
MacBook Pro <macbookpro@fid49ti5sa.local>
2016-02-04 20:53:23 -0600

    Initial Commit

---
+++ Prueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prueba
+//
+//  Created by MacBook Pro on 04/02/16.
+//  Copyright © 2016 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
